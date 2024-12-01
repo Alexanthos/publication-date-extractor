15b29236552a0e7415c2fc7b27af87ef54f721a5
zofiamilczarek <zmilcz@gmail.com>
2024-12-01 16:04:58 +0100

    added CLI to the preprocessing_finetune.py

--- preprocessing_finetune.py
+++ preprocessing_finetune.py
@@ -1,8 +1,11 @@
 import requests
 from datasets import load_dataset, Dataset
 import pandas as pd
+import argparse
+import os
 
-def get_text_doc(link, length = 4_000):
+
+def get_text_doc(link, length = 3_000):
     print(f'Fetching link {link}')
     def fetch_text(link):
         try:
@@ -36,7 +39,7 @@ def get_df_text_format(org_df_path, length):
     df_hf['text'] = df_hf['text version'].apply(lambda x : get_text_doc(x, length=length))
     return df_hf
 
-def get_dataset_text_format(org_df_path, length, dropna=True):
+def get_dataset_text_format(org_df_path, length, dropna=True, split = True):
     """
     Input:
         org_df_path: str, a path to a dataframe containing at least 2 colunns : url (urls to match the rows on) and 'text version' (the links to text versions of the documents)
@@ -50,5 +53,55 @@ def get_dataset_text_format(org_df_path, length, dropna=True):
         df_hf = df_hf.dropna()
     # creating a train-test split
     dataset = Dataset.from_pandas(df_hf)
-    dataset_train_test = dataset.train_test_split(test_size = 0.3, seed=42)
-    return dataset_train_test
+    if split:
+        dataset = dataset.train_test_split(test_size = 0.3, seed=42)
+    return dataset
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Preprocess dataset for finetuning or finetuned Llama inference.")
+    parser.add_argument("--input-path", type=str, help="Path to file that you want to label. Can be .csv or .pkl")
+    parser.add_argument("--out-type", type=str, default = 'csv', help="Output path and filename. Can be 'csv', 'pkl' (pandas Dataframe) or 'dataset' (HuggingFace DatasetDict)")
+    parser.add_argument("--output-path", type=str, help="The path where the output file will be saved")
+    parser.add_argument("--length", type=int, default= 3_000, help="The length of the start and end chunks, in characters")
+    parser.add_argument("--no-split", action='store_true', help = "When the script is run with this flag don't split the dataset into train and test")
+    
+    
+    args = parser.parse_args()
+    
+    assert args.out_type in ('csv', 'pkl', 'dataset'), f"Incorrect --out-type. Has to be one of ('csv', 'pkl', 'dataset'), not {args.out_type}"
+    
+    
+    dataset = get_dataset_text_format(args.input_path, args.length, split= not args.no_split)
+    
+    print(dataset)
+    
+    print(f'Saving the dataset to {args.output_path}')
+    
+    if args.out_type == 'dataset':
+        dataset.save_to_disk(args.output_path)
+    else:
+        if not args.no_split:
+            data_train, data_test = dataset['train'], dataset['test']
+            # making the directory where the dataframes will be saved
+            os.makedirs(args.output_path, exist_ok=True)
+            df_train, df_test = data_train.to_pandas(), data_test.to_pandas()
+            
+            train_path = os.path.join(args.output_path,f'df_train.{args.out_type}')
+            test_path = os.path.join(args.output_path,f'df_test.{args.out_type}')
+            
+            if args.out_type == 'csv':
+                df_train.to_csv(train_path)
+                df_test.to_csv(test_path)
+            elif args.out_type == 'pkl':
+                df_train.to_pickle(train_path)
+                df_test.to_pickle(test_path)
+        else:
+            df = dataset.to_pandas()
+            if args.out_type == 'csv':
+                df.to_csv(f"{args.output_path}.{args.out_type}")
+            elif args.out_type == 'pkl':
+                df.to_pickle(f"{args.output_path}.{args.out_type}")
+
+if __name__=='__main__':
+    main()
